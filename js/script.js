ea725f291ae0c018bab9efa968b82d46995361a3
JorgeNoolasco <0001237087@senaimgaluno.com.br>
2026-06-18 13:46:59 -0300

    nav bar
    implementei a funçao de clicar em botoes pra navegar no site

--- js/script.js
+++ js/script.js
@@ -9,3 +9,32 @@ cards.forEach(card => {
         card.style.transform = "translateY(0)";
     });
 });
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("mouseenter", () => {
+        card.style.transform = "translateY(-10px)";
+    });
+
+    card.addEventListener("mouseleave", () => {
+        card.style.transform = "translateY(0)";
+    });
+});
+
+const links = document.querySelectorAll(".menu a");
+
+links.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const destino = document.querySelector(
+            link.getAttribute("href")
+        );
+
+        if (destino) {
+            destino.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    });
+});
